861484756029c51446372a772900e9b9a3466496
Bratini89 <carlosbratini@hotmail.com>
2023-02-14 04:36:26 -0400

    Foto

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 import { FcBusinessman } from "react-icons/fc"
+import james from "../assets/james.jpg"
 const Main = () => {
   return (
     <div id="main">
       <img
         className="w-full h-screen object-cover object-left scale-x-[-1]"
-        src="/src/assets/james-harrison-vpOeXr5wmR4-unsplash.jpg"
+        src={james}
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
         <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
